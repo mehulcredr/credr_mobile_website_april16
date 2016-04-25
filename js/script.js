7bb71db82c88e08d42ea4915384744cbc51d97c4
Chirag Jani <Chirag@cma.local>
2016-04-25 11:16:38 +0530

    Styling for homepage

--- js/script.js
+++ js/script.js
@@ -1,93 +1,96 @@
-/*$('.dot').velocity({
-  rotateZ: '360deg', 'font-color': 'blue'}, {duration: 1000	});
-
-*/
-
-/*$('.loader').velocity({
-  perspective:[100, 50],
-  roatetZ:[5, 0],
-  perspective:[100, 50],
-},
-{duration: 800, delay: 3000, loop: 2
-});
+/**
+ *
+ * Left Side Menu
+ *
+ *
+ **/
 
 
+(function() {
 
-$('.dots').velocity({
-  opacity:[
-  function(){return Math.random()},
-  function(){return Math.random() + 0.1}
-  ],
-},
-{duration: 10000, delay: 3000, loop: 2
-});
-*/
+	var bodyEl = document.body,
+		content = document.querySelector( '.content-wrap' ),
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false;
 
-/*$('.arrow-dot')
-.velocity({
+	function init() {
+		initEvents();
+	}
 
-  translateX: "15px"
-},
- {
-  loop: true
-});
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleMenu );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenu );
+		}
 
-$('.square-dot').velocity({
-  rx: "7px",
-}, {
-  duration:300
-});
+		// close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleMenu();
+			}
+		} );
+	}
 
-$('.square-dot').velocity({
-	
-  opacity: 0.5,
+	function toggleMenu() {
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+		}
+		isOpen = !isOpen;
+	}
 
-  fill: "#D92229"
-}, {
-  loop: true
-});*/
+	init();
 
+})();
 
-/*$('.dots').velocity({
-  rotateZ: ["360deg"]
-},
-{loop:true
-});
-*/
+/**
+ *
+ * Login Overlay
+ *
+ *
+ **/
 
-$("rect")
-    .velocity({ fill: "#D92229"})
-    .delay(500)
-    .velocity({ rx: "10px",width: "10px", height: "10px"})
-    .delay(500)
+ (function() {
+	var triggerBttn = document.getElementById( 'trigger-overlay' ),
+		overlay = document.querySelector( 'div.overlay' ),
+		closeBttn = overlay.querySelector( 'button.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
 
-$('.arrow-dot')
-  .velocity({ stroke: "#ffffff", strokeWidth: "2px"})
+	function toggleOverlay() {
+		if( classie.has( overlay, 'open' ) ) {
+			classie.remove( overlay, 'open' );
+			classie.add( overlay, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				classie.remove( overlay, 'close' );
+			};
+			if( support.transitions ) {
+				overlay.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
+		}
+		else if( !classie.has( overlay, 'close' ) ) {
+			classie.add( overlay, 'open' );
+		}
+	}
 
-    .delay(300)
-    .velocity({ fill: "#ffffff"})
-
-    .delay(500)
-
-$('.arrow-dot')
-  .velocity({ translateX: "15px"
-},
-{
-  loop: true
-});
-
-
-$(".rotating-dot")
-  .delay(500)
-  .velocity({ x: "+=97.5", y: "-50" })
-  .delay(100)
-  .velocity({ x: "+=97.5", y: "50.5" })
-  .delay(100)
-  .velocity({ x: "-=97.5", y: "+155" })
-  .delay(100)
-  .velocity({ x: "-50", y: "50.5" })
-  .delay(100)
-
-
-
-
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
+})();
